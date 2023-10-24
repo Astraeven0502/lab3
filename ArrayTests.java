eddccb95437e2e64db1fc2a93a6f9058c417d88e
Astraeven <astraeven0502@gmail.com>
2023-10-23 17:29:54 -0700

    Debug ArrayExample and ArrayTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,20 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = { 3, 4, 5, 6, 7 }; 
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 7, 6, 5, 4, 3 }, input2);
 	}
+
+
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1, 2, 3, 4 };
+    assertArrayEquals(new int[]{4, 3, 2, 1 }, ArrayExamples.reversed(input1));
   }
+  
 }
